dc1ed7ae5321c8e5b19684d63bc8d3534164fb38
Mikayel Arzumanyan <mikarzum@c2r12s1.42yerevan.am>
2023-11-23 21:13:18 +0400

    das

--- ex00/BitcoinExchange.hpp
+++ ex00/BitcoinExchange.hpp
@@ -4,3 +4,16 @@
 #include <map>
 #include <fstream>
 
+class BitcoinExchange
+{
+	private:
+	public:
+		///TODO: partadir piti pirvate liner?
+		std::map<std::string, float> mp;
+	
+	public:
+		BitcoinExchange();
+		BitcoinExchange(const BitcoinExchange& obj);
+		BitcoinExchange& operator=(const BitcoinExchange& obj);
+		~BitcoinExchange();
+};
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,21 +1,43 @@
 #include "BitcoinExchange.hpp"
 
+// #include <string>
+
 int main(int argc, char** argv)
 {
 	try
 	{
-		///TODO: qu exception@ sarqi
+		///TODO: qu exception@ sarqi, coz subjct example
 		if (argc != 2)
 			throw (std::exception()); 
 
 		std::fstream data;
 		std::fstream inpt;
-		
+
 		data.open("./data.csv", std::ios_base::in);
 		// strm.is_open();
 		inpt.open(argv[1], std::ios_base::in);
 		// strm.is_open();
 
+		//////TODO: vrode ban chi lini ete class ch haytararem u tupo map@ ogtagortsem che?
+
+		BitcoinExchange dbase;
+		{
+			std::string	key;
+			float 		value;
+			char 		*buff1;
+			char 		*buff2;
+			// for (std::map<std::string, float>::iterator it = dbase.mp.begin(); it != dbase.mp.end(); ++it)
+			for (size_t i = 0; i < std::count(std::istreambuf_iterator<char>(inpt),std::istreambuf_iterator<char>(), '\n'); i++)
+			{
+
+				inpt.getline(buff1, ',');
+				inpt.getline(buff2, '\n');
+				
+				dbase.mp.insert(std::pair<std::string, float>(static_cast<std::string>(buff1), \
+				static_cast<std::string>(buff2).find_first_of("") != std::string::npos ? \
+				atoi(buff2) : static_cast<float>(atof(buff2))));
+			}	
+		}
 
 		inpt.close();
 		// strm.is_open();
